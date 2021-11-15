2bd9cf75afc909f57674e0d59fe0ca0af5fd87d0
shiro641 <70186086+shiro641@users.noreply.github.com>
2021-11-15 19:28:47 +0800

    update
    主页

--- front-end/src/main.js
+++ front-end/src/main.js
@@ -1,15 +1,20 @@
-// The Vue build version to load with the `import` command
-// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
-import App from './App'
+import App from './App.vue'
 import router from './router'
+import store from './store'
+import ElementUI from 'element-ui';
+import 'element-ui/lib/theme-chalk/index.css';
+import myHeader from './components/myHeader'
+import myCloud from './components/keywordsCloud'
+import './styles/font.css'
 
+Vue.component('mycloud', myCloud)
+Vue.component('myHeader', myHeader)
+Vue.use(ElementUI);
 Vue.config.productionTip = false
 
-/* eslint-disable no-new */
 new Vue({
-  el: '#app',
   router,
-  components: { App },
-  template: '<App/>'
-})
+  store,
+  render: h => h(App)
+}).$mount('#app')
---
+++ front-end/src/views/Home.vue
@@ -0,0 +1,53 @@
+<template>
+  <div id="app">
+    <mycloud></mycloud>
+  </div>
+</template>
+
+<script>
+import Vue from "vue";
+import VueScrollTo from "vue-scrollto";
+
+Vue.use(VueScrollTo, options);
+
+//https://github.com/rigor789/vue-scrollto
+
+let options = {
+  container: "body", //滚动的容器
+  duration: 500, //滚动时间
+  easing: "ease", //缓动类型
+  offset: 0, //滚动时应应用的偏移量。此选项接受回调函数
+  force: true, //是否应执行滚动
+  cancelable: true,
+  onStart: false,
+  onDone: false,
+  onCancel: false,
+  x: false,
+  y: true
+};
+
+export default {
+  name: "app",
+  data() {
+      return{
+          test:['a','b','c']
+      }
+  }
+};
+</script>
+
+<style>
+.menu {
+  position: fixed;
+  width: 200px;
+  background-color: #eee;
+}
+.content {
+  width: 500px;
+  height: 500px;
+  margin-left: 300px;
+  margin-bottom: 50px;
+  background-color: #f5f5f5;
+  border: 1px solid #ddd;
+}
+</style>
