f1529046b2b57c6a237038823da2bf6437ade7d2
hrx20000209 <417336070@qq.com>
2021-12-14 19:15:59 +0800

    合并finish

--- front-end/src/router/index.js
+++ front-end/src/router/index.js
@@ -9,7 +9,7 @@ const routes = [
   {
     path: '/',
     name: 'Home',
-    component: () => import('../wyh_home')
+    component: () => import('../views/homepage')
   },
   {
     path: '/homepage',
